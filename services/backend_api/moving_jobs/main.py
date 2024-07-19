56fbece026488afdc671dd297a321f39dbae1bd9
nat turner <jelj316@gmail.com>
2024-07-19 13:52:00 -0400

    update

--- services/backend_api/moving_jobs/main.py
+++ services/backend_api/moving_jobs/main.py
@@ -1,9 +1,20 @@
-from typing import Union
+import schemas
+import models
+from typing import Union, List
+from db.mlsdb import get_db, engine
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI
+
+
+
 
 app = FastAPI()
 
+app2 = FastAPI()
 
 @app.get("/")
 def read_root():
@@ -13,3 +24,44 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# app2.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
+
+@app2.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app2.post('/new_job', response_model=schemas.Job)
+
+def new_job(new_job: schemas.Job, db: Session = Depends(get_db)):
+    new_job_entry = models.add_new_job(db, new_job)
+
+
+    return new_job_entry
+
+models.Base.metadata.create_all(engine)   
+
+@app2.get('/jobs', response_model=List[schemas.UserDisplay])
+def get_all_jobs(db: Session = Depends(get_db)):
+    return models.get_all_jobs(db)
+
+@app2.get('/job/{id}', response_model=schemas.UserDisplay)
+def get_job(id: int, db: Session = Depends(get_db)):
+    return models.get_job(db, id)
+
+@app2.get('/job/{id}/bill')
+def bill_job(id: int, db: Session = Depends(get_db)):
+
+    job = models.get_job(db, id)
+    return models.bills(job)
+
+@app2.put('/job/{id}/update')
+def update_job(id: int, request: schemas.Job, db: Session = Depends(get_db)):
+    return models.update_job(db, id, request)
+
+@app2.delete('/job/{id}/delete')
+def delete_job(id: int, db: Session = Depends(get_db)):
+    return models.delete_job(db, id)
+
